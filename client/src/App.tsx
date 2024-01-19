523526331bf2f6ccc0988297288ba6edc4fa7ee3
Tim Engle <timothy.englew@gmail.com>
2024-01-19 09:14:24 -0800

    Add error display for invalid URL params

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,14 @@ import "./App.css";
 import ShortenerForm from "./Components/ShortenerForm/ShortenerForm";
 
 function App() {
+  const queryParams = new URLSearchParams(window.location.search);
+  const error = queryParams.get("error");
   return (
     <>
       <h1 className="mb-12 text-4xl font-bold tracking-wider text-blue-500">
         URL Shortener
       </h1>
+      {error && <p className="text-red-500">{error}</p>}
       <ShortenerForm />
     </>
   );
