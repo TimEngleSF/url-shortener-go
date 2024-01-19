c0e07fbf5a4250b7525eba4ac84cc681e2436fe0
Tim Engle <timothy.englew@gmail.com>
2024-01-18 18:22:35 -0800

    Add port env var

--- main.go
+++ main.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"link-short/controller"
+	"log"
+	"os"
 
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err !=nil{
+		log.Println("no .env file found")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environment variable")
+	}
+
 	r := gin.Default()
 
 	r.Use(static.Serve("/", static.LocalFile("./client/dist", true)))
@@ -16,5 +28,5 @@ func main() {
 	r.POST("/create", controller.Create)
 	r.GET("/:paramURL", controller.Redirect)
 
-	r.Run(":8000")
+	r.Run(":" + port)
 }
