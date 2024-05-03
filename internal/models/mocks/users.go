02cb1eb7d58da4044eab6db6cb4401f368477c8f
Tim Engle <timothy.englew@gmail.com>
2024-05-03 13:45:16 -0700

    add user model mocks

---
+++ internal/models/mocks/users.go
@@ -0,0 +1,25 @@
+package mocks
+
+import (
+	"context"
+
+	"github.com/TimEngleSF/url-shortener-go/internal/models"
+)
+
+type UserMock struct{}
+
+func (m *UserMock) Insert(ctx context.Context, name, email, password string) error {
+	return nil
+}
+
+func (m *UserMock) Authenticate(ctx context.Context, email, password string) (*models.User, error) {
+	return nil, nil
+}
+
+func (m *UserMock) ExistsByID(ctx context.Context, id int) (bool, error) {
+	return false, nil
+}
+
+func (m *UserMock) ExistsByEmail(ctx context.Context, email string) (bool, error) {
+	return false, nil
+}
