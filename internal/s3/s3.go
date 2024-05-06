c7c09edf16eaf31259bd24d8e66c714a243f336d
Tim Engle <timothy.englew@gmail.com>
2024-05-05 22:15:03 -0700

    add s3 interface, mocks, and UploadFile method

---
+++ internal/s3/s3.go
@@ -0,0 +1,55 @@
+package S3
+
+import (
+	"bytes"
+	"context"
+	"log"
+
+	"github.com/TimEngleSF/url-shortener-go/internal/helper"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+type S3Interface interface {
+	UploadFile(ctx context.Context, key string, img []byte) error
+}
+
+type S3 struct {
+	Client *s3.Client
+}
+
+var (
+	s3Client *S3
+	bucket   string
+	region   string
+)
+
+func NewS3Client() (*s3.Client, error) {
+	// Create an AWS config
+	if region == "" || bucket == "" {
+		region = helper.GetEnv("AWS_REGION")
+		bucket = helper.GetEnv("S3_BUCKET")
+	}
+
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := s3.NewFromConfig(cfg)
+
+	return client, nil
+}
+
+func (s *S3) UploadFile(ctx context.Context, key string, img []byte) error {
+	r := bytes.NewReader(img)
+	_, err := s.Client.PutObject(ctx, &s3.PutObjectInput{
+		Bucket: &bucket,
+		Key:    &key,
+		Body:   r,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
